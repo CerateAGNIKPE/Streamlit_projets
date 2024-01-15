53430b0e71edbd000162643f0db7b386cfd8ad1c
CerateAGNIKPE <125263974+CerateAGNIKPE@users.noreply.github.com>
2024-01-15 16:20:21 +0100

    Update Cloud_nuage.py

--- projet_st/Cloud_nuage.py
+++ projet_st/Cloud_nuage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import os 
 from mask_display import Display_choice
 import random 
