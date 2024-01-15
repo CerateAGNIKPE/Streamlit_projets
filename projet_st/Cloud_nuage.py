59da3a17d01e1c4500e8c802e53c2de747def37d
CerateAGNIKPE <125263974+CerateAGNIKPE@users.noreply.github.com>
2024-01-15 17:56:25 +0100

    Update Cloud_nuage.py

--- projet_st/Cloud_nuage.py
+++ projet_st/Cloud_nuage.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import os 
 import random 
-#from mask_display import Display_choice
+from mask_display import Display_choice
 
 train_folder_path = 'projet_st/train_images'
 #test_folder_path = 'test_images'
@@ -103,5 +103,13 @@ if page == pages[1]:
         st.image('projet_st/label.jpg')
         st.image('projet_st/nb_label.jpg')
 
-
+    # page : Show some images & masks
+    elif chapter == chapters[3] :
+            display_option = st.selectbox(label = "Display images & masks", 
+                                options = ['Display_four_simple_mask_multi_images', 'Display_multiple_masks_multi_images',
+                                            'Display_four_simple_bbox_multi_images', 'Display_multiple_bboxes_multi_images'
+                                            ])
+            if st.checkbox("Get a new image") : 
+                random.shuffle(train_image_files) # get ramdom images
+            st.pyplot(Display_choice(display_option, train_image_files))
        
